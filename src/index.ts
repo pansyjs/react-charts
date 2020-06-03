5eb8bd26575903cb4b4034c3b4d665016a952dc3
kang <wang_xingkang@qq.com>
2020-06-03 10:46:22 +0800

    feat: add RangeColumn、GroupedColumn

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ export { default as Line } from './line';
 export { default as Area } from './area';
 export { default as Pie } from './pie';
 export { default as Dount } from './donut';
-export { default as Column } from './column';
 export { default as StepLine } from './step-line';
 export { default as Bar } from './bar';
 export { default as Bubble } from './bubble';
@@ -16,7 +15,12 @@ export { default as Funnel } from './funnel';
 export { default as Gauge } from './gauge';
 export { default as Waterfall } from './waterfall';
 export { default as Treemap } from './treemap';
+
+// 柱状图
+export { default as Column } from './column';
+export { default as RangeColumn } from './range-column';
 export { default as StackedColumn } from './stacked-column';
+export { default as GroupedColumn } from './grouped-column';
 
 // 迷你图表
 export { default as Progress } from './progress';
