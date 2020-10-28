d656d1518e8c28ad135dd8d3c983f50bc86b16e6
wangxingkang <156148958@qq.com>
2020-10-28 12:55:35 +0800

    docs(waterfall): add example

---
+++ docs/components/waterfall/demo/demo-01.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Waterfall } from '@pansy/react-charts';
+import { WaterfallConfig } from '@pansy/react-charts/es/waterfall';
+
+const data = [
+  { type: '日用品', money: 120 },
+  { type: '伙食费', money: 900 },
+  { type: '交通费', money: 200 },
+  { type: '水电费', money: 300 },
+  { type: '房租', money: 1200 },
+  { type: '商场消费', money: 1000 },
+  { type: '红包收入', money: -2000 }
+];
+
+export default () => {
+  const config: WaterfallConfig = {
+    data,
+    xField: 'type',
+    yField: 'money',
+    appendPadding: [15, 0, 0, 0],
+    meta: {
+      type: { alias: '类别' },
+      money: {
+        alias: '收支',
+        formatter: (v) => `${v} 元`
+      }
+    },
+    label: {
+      style: {
+        fontSize: 10,
+        fill: 'rgba(0,0,0,0.65)'
+      },
+      layout: [{ type: 'interval-adjust-position' }]
+    },
+    total: {
+      label: '总支出',
+      style: { fill: '#96a6a6' }
+    }
+  };
+  return <Waterfall {...config} />;
+};
---
+++ docs/components/waterfall/demo/demo-02.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import { Waterfall } from '@pansy/react-charts';
+import { WaterfallConfig } from '@pansy/react-charts/es/waterfall';
+
+const data = [
+  { month: '2019', value: 23000000 },
+  { month: 'Jan', value: 2200000 },
+  { month: 'Feb', value: -4600000 },
+  { month: 'Mar', value: -9100000 },
+  { month: 'Apr', value: 3700000 },
+  { month: 'May', value: -2100000 },
+  { month: 'Jun', value: 5300000 },
+  { month: 'Jul', value: 3100000 },
+  { month: 'Aug', value: -1500000 },
+  { month: 'Sep', value: 4200000 },
+  { month: 'Oct', value: 5300000 },
+  { month: 'Nov', value: -1500000 },
+  { month: 'Dec', value: 5100000 }
+];
+
+export default () => {
+  const config: WaterfallConfig = {
+    data,
+    padding: 'auto',
+    appendPadding: [20, 0, 0, 0],
+    xField: 'month',
+    yField: 'value',
+    meta: {
+      month: {
+        alias: '月份'
+      },
+      value: {
+        alias: '销售量',
+        formatter: (v) => `${v / 10000000} 亿`
+      }
+    },
+    /** 展示总计 */
+    total: {
+      label: '2020'
+    },
+    color: ({ month, value }) => {
+      if (month === '2019' || month === '2020') {
+        return '#96a6a6';
+      }
+      return value > 0 ? '#f4664a' : '#30bf78';
+    },
+    legend: {
+      custom: true,
+      items: [
+        {
+          name: 'Increase',
+          value: 'increase',
+          marker: { symbol: 'square', style: { r: 5, fill: '#f4664a' } }
+        },
+        {
+          name: 'Decrease',
+          value: 'decrease',
+          marker: { symbol: 'square', style: { r: 5, fill: '#30bf78' } }
+        },
+        {
+          name: 'Total',
+          value: 'total',
+          marker: { symbol: 'square', style: { r: 5, fill: '#96a6a6' } }
+        }
+      ]
+    },
+    label: {
+      style: {
+        fontSize: 10
+      },
+      layout: [{ type: 'interval-adjust-position' }]
+    },
+    waterfallStyle: () => {
+      return {
+        fillOpacity: 0.85
+      };
+    }
+  };
+  return <Waterfall {...config} />;
+};
