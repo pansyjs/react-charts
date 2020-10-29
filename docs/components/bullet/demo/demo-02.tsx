017d7841de7abc171fe8dee7ddd0dc08d8b5e9c4
wangxingkang <156148958@qq.com>
2020-10-29 13:17:37 +0800

    docs(bullet): 完善文档

---
+++ docs/components/bullet/demo/demo-02.tsx
@@ -0,0 +1,63 @@
+/**
+ * title: 单折线图的基础用法
+ * desc: 最基础简单的折线图使用方式，显示一个指标的趋势
+ */
+import React from 'react';
+import { Bullet } from '@pansy/react-charts';
+import { BulletConfig } from '@pansy/react-charts/es/bullet';
+
+const data = [
+  {
+    title: '满意度',
+    ranges: [100],
+    measures: [80],
+    target: 85
+  }
+];
+
+export default () => {
+  const config: BulletConfig = {
+    data,
+    measureField: 'measures',
+    rangeField: 'ranges',
+    targetField: 'target',
+    xField: 'title',
+    color: {
+      range: '#5B8FF9',
+      measure: '#5B8FF9',
+      target: '#5B8FF9'
+    },
+    xAxis: {
+      line: null
+    },
+    yAxis: false,
+    layout: 'vertical',
+    label: {
+      measure: {
+        position: 'middle',
+        style: {
+          fill: '#fff'
+        }
+      }
+    },
+    // 自定义 legend
+    legend: {
+      custom: true,
+      position: 'bottom',
+      items: [
+        {
+          value: '实际值',
+          name: '实际值',
+          marker: { symbol: 'square', style: { fill: '#5B8FF9', r: 5 } }
+        },
+        {
+          value: '目标值',
+          name: '目标值',
+          marker: { symbol: 'line', style: { stroke: '#5B8FF9', r: 5 } }
+        }
+      ]
+    }
+  };
+
+  return <Bullet {...config} />;
+};
---
+++ docs/components/bullet/demo/demo-05.tsx
@@ -0,0 +1,95 @@
+/**
+ * title: 单折线图的基础用法
+ * desc: 最基础简单的折线图使用方式，显示一个指标的趋势
+ */
+import React from 'react';
+import { Bullet } from '@pansy/react-charts';
+import { BulletConfig } from '@pansy/react-charts/es/bullet';
+
+const data = [
+  {
+    title: '重庆',
+    ranges: [30, 90, 120],
+    measures: [65],
+    target: 80
+  },
+  {
+    title: '杭州',
+    ranges: [30, 90, 120],
+    measures: [50],
+    target: 100
+  },
+  {
+    title: '广州',
+    ranges: [30, 90, 120],
+    measures: [40],
+    target: 85
+  },
+  {
+    title: '深圳',
+    ranges: [30, 90, 120],
+    measures: [50],
+    target: 100
+  }
+];
+
+export default () => {
+  const config: BulletConfig = {
+    data,
+    measureField: 'measures',
+    rangeField: 'ranges',
+    targetField: 'target',
+    xField: 'title',
+    color: {
+      range: ['#FFB1AC', '#FFDBA2', '#B4EBBF'],
+      measure: '#5B8FF9',
+      target: '#5B8FF9'
+    },
+    label: {
+      measure: {
+        position: 'middle',
+        style: {
+          fill: '#fff'
+        }
+      }
+    },
+    xAxis: {
+      line: null
+    },
+    yAxis: false,
+    // 自定义 legend
+    legend: {
+      custom: true,
+      position: 'bottom',
+      items: [
+        {
+          value: '差',
+          name: '差',
+          marker: { symbol: 'square', style: { fill: '#FFB1AC', r: 5 } }
+        },
+        {
+          value: '良',
+          name: '良',
+          marker: { symbol: 'square', style: { fill: '#FFDBA2', r: 5 } }
+        },
+        {
+          value: '优',
+          name: '优',
+          marker: { symbol: 'square', style: { fill: '#B4EBBF', r: 5 } }
+        },
+        {
+          value: '实际值',
+          name: '实际值',
+          marker: { symbol: 'square', style: { fill: '#5B8FF9', r: 5 } }
+        },
+        {
+          value: '目标值',
+          name: '目标值',
+          marker: { symbol: 'line', style: { stroke: '#5B8FF9', r: 5 } }
+        }
+      ]
+    }
+  };
+
+  return <Bullet {...config} />;
+};
